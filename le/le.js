4927e07c8356ef8aac20e26e2a108539da4e8bc5
Erty Seidel <ertysdl+github@gmail.com>
2013-07-18 17:33:17 +0000

    Started work on level editor

--- le/le.js
+++ le/le.js
@@ -1,3 +1,15 @@
 ;(function(exports){
-	var ctx = document.getElementById("canvas").getContext("2d")
+
+	var ctx = document.getElementById("canvas").getContext("2d");
+
+
+
+	var loop = function(){
+		draw();
+		requestAnimationFrame(loop);
+	}
+
+	this.exports.loop = loop;
 })(this);
+
+requestAnimationFrame(loop);
